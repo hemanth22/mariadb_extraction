345dc1a9786478e1a2d6775ad18651e36f5f4998
Hemanth B <hemanth22hemu@gmail.com>
2022-08-15 11:39:42 +0530

    feat: creating python code

---
+++ pythonmariadb.py
@@ -0,0 +1,28 @@
+import mysql.connector as mysql
+import sys
+import csv
+
+def connect(db_name, dbhost, dbuser, dbpass, dbport):
+    try:
+        return mysql.connect(
+            host=dbhost,
+            user=dbuser,
+            password=dbpass,
+            database=db_name,
+            port=dbport)
+    except Error as e:
+        print(e)
+
+if __name__ == '__main__':
+    db = connect(databasename,databasehost,databaseusername,databasepassword,databaseport)
+    f = open("test.csv", "w")
+    cursor = db.cursor()
+    fields = ['id', 'ask', 'bid', 'creationdate'] # field names 
+    dbquery = "SELECT `id`, `ask`, `bid`, `creationdate` FROM `publishedrates`" # query
+    cursor.execute(dbquery)
+    project_records = cursor.fetchall() # fetch all records
+    outputfile = csv.writer(f, lineterminator='\n') # create the csv writer object
+    outputfile.writerow(fields) # write the fields
+    outputfile.writerows(project_records) # write the data
+    print(project_records)
+    db.close()
