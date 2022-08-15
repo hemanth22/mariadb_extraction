9ecc41f8768b24b528969902c9a172b3f2d38209
Hemanth B <hemanth22hemu@gmail.com>
2022-08-15 07:54:18 +0000

    feat: email job

---
+++ sendingmail.py
@@ -0,0 +1,47 @@
+# import the necessary components first
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+port=465
+smtp_server = os.environ.get("SMTP_ADDRESS")
+USER_EMAIL = os.environ.get("USER_EMAIL")
+USER_PASSWORD = os.environ.get("USER_PASSWORD")
+sender_email = "hemanth22hemu@gmail.com "
+receiver_email = "hemanthbitra@live.com"
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "[High] keepalive_jfrog status"
+message["From"] = sender_email
+message["To"] = receiver_email
+# write the plain text part
+text = """\
+|INFORMATION|
+
+mariadb_extraction job completed now.
+
+Thanks,
+Information Mangement."""
+# write the HTML part
+html = """\
+<html>
+  <body>
+<h1 style="text-align: center;"><span style="color: #ff0000;"><strong>|INFORMATION|</strong></span></h1>
+<h3 style="text-align: center;">mariadb_extraction job completed now.</h3>
+<p style="text-align: center;"><strong>Thanks,</strong></p>
+<p style="text-align: center;"><strong>Information Mangement.</strong></p>
+  </body>
+</html>
+"""
+# convert both parts to MIMEText objects and add them to the MIMEMultipart message
+part1 = MIMEText(text, "plain")
+part2 = MIMEText(html, "html")
+message.attach(part1)
+message.attach(part2)
+# send your email
+with smtplib.SMTP_SSL(smtp_server,port) as mailserver:
+    mailserver.login(USER_EMAIL,USER_PASSWORD)
+    mailserver.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
+print('Sent')
