791bddacd0e9658eb0b03d4cfdcb726c74b109c4
Hemanth B <hemanth22hemu@gmail.com>
2022-08-15 10:35:35 +0000

    feat: attachment

--- sendingmail.py
+++ sendingmail.py
@@ -21,7 +21,7 @@ message["Subject"] = "[High] mariadb_extraction status"
 message["From"] = sender_email
 message["To"] = receiver_email
 
-msg.attach(MIMEText(file("test.csv").read()))
+message.attach(MIMEText(file("test.csv").read()))
 
 # write the plain text part
 text = """\
